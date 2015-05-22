2eba73ac65e99cae13c91d00a942777efcd23103
Steven Shaw <steven@steshaw.org>
2015-05-23 00:35:42 +1000

    Add simple expression parser. Run on each command line argument

--- src/Main.hs
+++ src/Main.hs
@@ -1,1 +1,22 @@
-main = putStrLn "Hello!"
+import System.Environment
+import Control.Monad (forM_)
+import Text.ParserCombinators.Parsec
+import qualified Text.ParserCombinators.Parsec.Number as Numbers
+
+expr = chainl1 int infixOp
+
+int = Numbers.int
+
+infixOp = do
+  spaces
+  char '+'
+  spaces
+  return (+)
+
+runExpr = runParser expr 0 "<input>"
+
+main = do
+  putStrLn "args: {"
+  args <- System.Environment.getArgs
+  args `forM_` (putStrLn . show . runExpr)
+  putStrLn "}"
