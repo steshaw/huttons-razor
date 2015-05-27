1865af3092dd930926edc29865ee5d0cbf658e7f
Steven Shaw <steven@steshaw.org>
2015-05-27 22:18:04 +1000

    Introduce -Wall and fix warnings

--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,26 @@
 import System.Environment (getArgs)
-import Control.Monad (forM_)
-import Text.ParserCombinators.Parsec (spaces, char, chainl1, runParser)
+import Text.ParserCombinators.Parsec (Parser, ParseError, spaces, char, chainl1, runParser)
 import Text.ParserCombinators.Parsec.Number (int)
 
+infixOp :: Parser (Integer -> Integer -> Integer)
 infixOp = do
   spaces
-  char '+'
+  _ <- char '+'
   spaces
   return (+)
 
+expr :: Parser Integer
 expr = chainl1 int infixOp
 
-runExpr = runParser expr 0 "<input>"
+evalExpr :: String -> Either ParseError Integer
+evalExpr = runParser expr () "<input>"
 
-showResult (Left a)  = "Error: " ++ show a
-showResult (Right a) = show a
+showResult :: Either ParseError Integer -> String
+showResult = either (("Error: " ++) . show) show
 
-parse = (" - " ++) . showResult . runExpr
-
+main :: IO ()
 main = do
   putStrLn "args:"
   args <- System.Environment.getArgs
-  mapM_ (putStrLn . parse) args
+  mapM_ (putStrLn . (" - " ++) . showResult . evalExpr) args
   putStrLn ""
