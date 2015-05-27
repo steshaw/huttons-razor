feb8b37e27222452fefa11d95684fc99ec03afa3
Steven Shaw <steven@steshaw.org>
2015-05-27 16:57:25 +1000

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,11 +1,12 @@
 { mkDerivation, base, parsec, parsec-numbers, stdenv }:
 mkDerivation {
   pname = "huttons-razor";
-  version = "0.1.0.0";
+  version = "0.1.1.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
   buildDepends = [ base parsec parsec-numbers ];
+  homepage = "https://github.com/steshaw/huttons-razor";
   description = "Quick implemention of Hutton's Razor";
   license = stdenv.lib.licenses.bsd2;
 }
